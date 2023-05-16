11f6b4c98ba902041323b60be0d153004f1475a2
Al <breath44@yandex.ru>
2023-05-16 20:43:57 +0300

    77

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-print('hello world')
+class Point:
+    color='red'
+    circle=2
+
+Point.color='black'
+
+print(Point.__dict__)
+
+a=Point()
+a.color='oo'
+
+print(a.__dict__)
+
+b=Point()
+
+print(b.color)
+print(a.color)
