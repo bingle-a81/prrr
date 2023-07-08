dd08756f57cabd50ee562a8564ff2e8fa83077bb
Al <breath44@yandex.ru>
2023-07-08 09:28:28 +0300

    55

--- 1.py
+++ 1.py
@@ -1,12 +1,2 @@
-class P:
-    op=True
-    io=False
-
-    def set_coord():
-        print('вызов метода set coord')
-
-
-P.set_coord()
-
-pr=P()
-pr.set_coord()
+import pyautogui 
+pyautogui.moveTo(106, 464)
